d62c283a74f7781b9981bb8b5da7d4e83edba3dc
rock-007 <59394025+rock-007@users.noreply.github.com>
2021-01-24 19:53:14 +0000

    update

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ function add(n1, n2, showResult) {
     }
     return n1 + n2;
 }
+var person = {
+    name: 'umair',
+    age: 36,
+    hobbies: ['sports', 'Cooking']
+};
 var number1 = 5;
 var number2 = 2.8;
 var printResult = true;
 var result = add(number1, number2, printResult);
 console.log(result);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -17,6 +17,33 @@ return n1+n2;
 
 }
 
+
+// const person:{
+// name:string;
+// age:number;
+// hobbies:string[];
+// role:[number,string,number] // its Tuple  limit in the inputs
+
+// } ={
+// name: 'umair',
+// age:36,
+// hobbies:  ['sports', 'Cooking' ],
+// role:[2,'author',3]
+// }
+
+
+enum  Role { ADMIN=3, READ_ONLY,AUTHOR };
+const person = {
+  name: "umair",
+  age: 36,
+  hobbies: ["sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+
+person.role.push('admin');
+
+
 const number1: number=5;
 const number2=2.8;
 const printResult=true;
@@ -24,3 +51,10 @@ const printResult=true;
 const result =add(number1,number2, printResult);
 
 console.log(result);
+
+for(const hobby of person.hobbies){
+
+console.log(hobby);
+
+
+}
